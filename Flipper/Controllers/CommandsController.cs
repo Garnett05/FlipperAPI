df81fc046ef214542b217f0fc2f5d78f38340af7
Garnett05 <charlleslimamota10@hotmail.com>
2020-09-26 17:19:36 -0300

    Mockando informações na requisição get para verificar se a API está funcionando e retornando valores

---
+++ Flipper/Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flipper.Data;
+using Flipper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flipper.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]        
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+        //GET api/commands/{id
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Flipper/Data/ICommanderRepo.cs
@@ -0,0 +1,11 @@
+﻿using Flipper.Models;
+using System.Collections.Generic;
+
+namespace Flipper.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
